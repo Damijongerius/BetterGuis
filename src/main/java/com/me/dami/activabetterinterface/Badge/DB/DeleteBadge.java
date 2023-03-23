5fb0fbeb81b4154308c0f0d4a985c08400e49af8
DamiJ <damianojongerius@gmail.com>
2023-03-23 23:51:42 +0100

    implements delete and update

--- src/main/java/com/me/dami/activabetterinterface/Badge/DB/DeleteBadge.java
+++ src/main/java/com/me/dami/activabetterinterface/Badge/DB/DeleteBadge.java
@@ -1,4 +1,37 @@
 package com.me.dami.activabetterinterface.Badge.DB;
 
-public class DeleteBadge {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteBadge extends Connector {
+
+    public void RemoveBadge(int id) throws SQLException {
+        PreparedStatement statement = Connection().prepareStatement("DELETE FROM tp_Badge WHERE id = ?");
+            statement.setInt(1, id);
+            statement.executeUpdate();
+    }
+
+    /**
+     * @SQL this will delete all saved badges to kingdoms
+     */
+    public void RemoveKingdomBadges(String kingdom) throws SQLException {
+        PreparedStatement statement = Connection().prepareStatement("DELETE FROM KingdomBadge WHERE name = ?");
+        statement.setString(1, kingdom);
+        statement.executeUpdate();
+    }
+
+    /**
+     * @SQL this will delete every a badge from every kingdom
+     */
+    public void RemoveKingdomBadges(int id) throws SQLException {
+        PreparedStatement statement = Connection().prepareStatement("DELETE FROM KingdomBadge WHERE tp_Badge_ID = ?");
+        statement.setInt(1, id);
+        statement.executeUpdate();
+    }
+
+    public void RemovePlayerBadges(String player) throws SQLException {
+        PreparedStatement statement = Connection().prepareStatement("DELETE FROM PlayerBadge WHERE Player_UUID = ?");
+        statement.setString(1, player);
+        statement.executeUpdate();
+    }
 }
--- src/main/java/com/me/dami/activabetterinterface/Badge/DB/UpdateBadge.java
+++ src/main/java/com/me/dami/activabetterinterface/Badge/DB/UpdateBadge.java
@@ -1,4 +1,17 @@
 package com.me.dami.activabetterinterface.Badge.DB;
 
-public class UpdateBadge {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UpdateBadge extends Connector {
+
+    public void UpdateBadge(int id, String name, String Description) throws SQLException {
+        PreparedStatement pstmt = Connection().prepareStatement("UPDATE  SET Name = ?, Description = ? WHERE ID = ?");
+
+            pstmt.setString(1, name);
+            pstmt.setString(2, Description);
+            pstmt.setInt(3, id);
+
+            pstmt.executeUpdate();
+        }
 }
